0b3d51cdd5844fbb317d9c4d3c69093cffccd9b2
JuanPineda1985 <71187053+JuanPineda1985@users.noreply.github.com>
2020-09-15 20:20:17 -0500

    update propiedades

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -5,7 +5,34 @@ namespace OneIntro
     class GestionHotel
     {   
         //Propiedades
-        static void Main(string[] args)
+        public string habitacion { get; set;}
+        public int huesped {get; set;}
+
+        int Dias;
+        //public int huesped {get; set;}
+        public int dias{
+            get{}
+            set
+            {
+                if (value >=1)
+                {
+                    Dias = value;
+                }else
+                {
+
+                    Consle.WriteLine("Cantidad dias debe ser minimo 1");
+                }
+            }
+        }
+        
+        //Metodos
+        public int valorEstadia(int huesp, int cantd){
+            int TINDIVIDUAL  2500;
+            int TDOBLE = 4600;
+            int TFAMILIAR = 5200;
+        }
+
+
         {
             Console.WriteLine("Hello World!");
         }

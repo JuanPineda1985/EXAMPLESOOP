e999decddd1e119abd97d51174eca36bb8129fda
JuanPineda1985 <71187053+JuanPineda1985@users.noreply.github.com>
2020-10-05 14:29:55 -0500

    continuacion Evaluacion

---
+++ Eventos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Eventos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eventos");
+            Console.WriteLine("Exposicion: ");
+            string Exposicion = Console.ReadLine();
+            Console.WriteLine("Conferencia: ");
+            string Conferencia = Console.ReadLine();
+            Console.WriteLine("aforo: ");
+            int aforo = int.Parse(Console.ReadLine());
+
+            Cultural FiestaLibro = new Cultural()
+        }    
+        
+    }
+}
--- Eventos/obj/Debug/netcoreapp3.1/Eventos.AssemblyInfo.cs
+++ Eventos/obj/Debug/netcoreapp3.1/Eventos.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("oneintro")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Eventos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("oneintro")]
-[assembly: System.Reflection.AssemblyTitleAttribute("oneintro")]
+[assembly: System.Reflection.AssemblyProductAttribute("Eventos")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Eventos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.

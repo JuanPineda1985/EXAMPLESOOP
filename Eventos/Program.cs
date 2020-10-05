05656bcecc232b9e0bbe79156dbf295e01a9d148
JuanPineda1985 <71187053+JuanPineda1985@users.noreply.github.com>
2020-10-05 16:44:34 -0500

    Final de Ejercicio

--- Eventos/Program.cs
+++ Eventos/Program.cs
@@ -13,16 +13,34 @@ namespace Eventos
             string Confe = Console.ReadLine();
             Console.WriteLine("aforo: ");
             int Aforo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Productos en venta");
+            string Product = Console.ReadLine();
+            Console.WriteLine("Presentacion Empresarial");
+            string Social = Console.ReadLine();
+            Console.WriteLine("Muestras de Productos");
+            string muestras = Console.ReadLine();
 
-            var FiestaLibro = new Cultural()
+            Cultural cultural = new Cultural()
             {
                 Exposicion = Expo,
                 Conferencia = Confe,
                 Aforo = Aforo
             };
+            
+            Empresarial empresarial = new Empresarial ()
+            {
+               Venta_produc = Product,
+               Sociali_empresa = Social
 
-            Console.WriteLine($"{FiestaLibro.Exposicion} {FiestaLibro.Aforo}");
+            };
+
+            feria Feria = new feria()
+            {
+               Mostrario = muestras 
+            };
+
+
+            Console.WriteLine($"{cultural.Exposicion} {cultural.Exposicion} {cultural.Aforo} {empresarial.Venta_produc}  {empresarial.Sociali_empresa} {Feria.Mostrario}");
         }    
-        
     }
 }
